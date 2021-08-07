57d35979bf80a249d84f2721b0cc4cc4ac1bedbb
george200150 <georgeciubotariu@yahoo.com>
2021-08-07 12:38:21 +0300

    created classes

---
+++ src/main/java/com/george200150/fast_task/exceptions/JsonFormatException.java
@@ -0,0 +1,4 @@
+package com.george200150.fast_task.exceptions;
+
+public class JsonFormatException {
+}
---
+++ src/main/java/com/george200150/fast_task/parser/JsonImporter.java
@@ -0,0 +1,7 @@
+package com.george200150.fast_task.parser;
+
+public class JsonImporter {
+    public static void importList(String fileName) {
+
+    }
+}
