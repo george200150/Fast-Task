2c49ca9959553b17cbc2d8cae9ebd8a8fa19651a
irimialarisa22 <ieae0070@scs.ubbcluj.ro>
2021-08-07 15:29:23 +0300

    added buttons to the interface

--- src/main/java/com/george200150/fast_task/MainActivity.java
+++ src/main/java/com/george200150/fast_task/MainActivity.java
@@ -2,13 +2,26 @@ package com.george200150.fast_task;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonViewTasks;
+    private Button buttonAddTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonViewTasks = (Button) findViewById(R.id.button_view_tasks);
+        buttonAddTask = (Button) findViewById(R.id.button_add_task);
+
+        buttonViewTasks.setOnClickListener(view ->
+                Log.d("YOUR TAG", "George!")
+        );
     }
 }
